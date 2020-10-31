0116f1f9a471019d4836c7d4f487247f1d4b1c04
Anuj Rajak <mr.anujrajak@gmail.com>
2020-10-31 19:19:57 +0530

    adding procfile

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const express = require('express');
 
 const app = express();
 
+let port = process.env.PORT || 3000;
+
 app.get('/', (request, response) => {
     const res = lib.getResponse(request.url);
     response.setHeader('Content-Type', 'application/json');
     response.end(JSON.stringify(res, null, 4));
 });
 
-app.listen('1212', () => {
-    console.log('\nlorem ipsum\n');
+app.listen(port, () => {
+    console.log('\nlorem ipsum\n ');
 });
